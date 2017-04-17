9d61dea47c557bba48c8aeaccb58029f6d47f594
Andriy-sth <sht.andriy@gmail.com>
2017-04-17 17:50:52 +0300

    favorite start

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from music import views
+
 urlpatterns = [
+    #home
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls'))
 ]
