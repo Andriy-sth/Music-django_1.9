da6d7d17ebb213cd70da45685fb07b74b46f6cff
Andriy-sth <sht.andriy@gmail.com>
2017-04-22 12:38:51 +0300

    add registration form and login

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
-from music import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #home
@@ -9,3 +9,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
